f0dca6aaba985af1339a7cc58d21c783c18e6f44
Ngu Jason <ngujason94@gmail.com>
2021-07-02 21:47:00 -0400

    Time: 1 ms (98.73%), Space: 37.4 MB (33.13%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(st.isEmpty()) st.push(c);
+            
+            else if(c == ')' && st.peek() == '(') st.pop();
+            else if(c == '}' && st.peek() == '{') st.pop();
+            else if(c == ']' && st.peek() == '[') st.pop();
+            else st.push(c);
+        }
+        
+        return st.isEmpty() ? true : false;
+        
+    }
+}
