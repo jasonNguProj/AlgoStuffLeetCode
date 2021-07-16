d26b9fdaa123dddf4b1099a504fb43f04a0ebb77
Ngu Jason <ngujason94@gmail.com>
2021-07-16 19:05:45 -0400

    Time: 0 ms (100.00%), Space: 38.5 MB (70.67%) - LeetHub

---
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        
+        double prod = 1;
+        for (int i = 0; i < nums.length; i++){
+            prod *= nums[i];
+        }
+        
+        if(prod  > 0){
+            return 1;
+        }else if(prod < 0){
+            return -1;
+        } else{
+             return 0;
+        }   
+        
+    }
+}
