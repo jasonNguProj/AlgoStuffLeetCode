4a4891d592279c04e628990dadaa4a3c55c942a2
Ngu Jason <ngujason94@gmail.com>
2021-07-18 15:05:57 -0400

    Time: 2 ms (97.97%), Space: 48 MB (49.61%) - LeetHub

---
+++ count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+to solve this question we will use dfs recursion and traverse via the left and right node
+create a good node variable that we will increment whenever the present rootValue
+is greater or equal to the previous rootvalue then , 
+we take the max btw the prev and present max value to rep our new max value,
+then recursively call the fxn on the left and right sub tree pasiing in therie values
+so we will do it in 2 diff fxn , the dfs fxn and the main fxn and initialze the running val to Integer.MIN_VALUE
+and also initialize the goodnode variable as a class memebr on the global level and in the end return the goodNodes
+*/
+class Solution {
+    int goodNode;
+    public int goodNodes(TreeNode root) {
+        goodNode = 0;
+        dfs(root, Integer.MIN_VALUE);
+        return goodNode;
+        
+    }
+    
+    private void dfs(TreeNode root, int max){
+        
+        if(root == null) return;
+        
+        if(root.val >= max){
+            goodNode++;
+        }
+          max = Math.max(root.val, max);
+        dfs(root.left, max);
+        dfs(root.right, max);
+    }
+    
+    
+}
