4bcbb132eb0422b7f262b068e61c5f2412f6e5b9
Ngu Jason <ngujason94@gmail.com>
2021-07-27 23:42:47 -0400

    Time: 34 ms (66.17%), Space: 42 MB (96.61%) - LeetHub

--- count-primes/count-primes.java
+++ count-primes/count-primes.java
@@ -5,18 +5,20 @@ so 3 steps we have a boolean array
 we traverse from 2 the first prime
 then set its square to true 
 then we traverse to count the ones that are still false
+this takes O(N) space | O(loglog n)
 */
 
 class Solution {
     public int countPrimes(int n) {
         
         boolean[] sqNum = new boolean[n];
-        for(int i = 0; i <sqNum.length; i++){
-            sqNum[i] = true;
-        }
+        // for(int i = 0; i <sqNum.length; i++){
+        //     sqNum[i] = true;
+        // }
+        Arrays.fill(sqNum, true);
         
-        for(int i = 2; i * i < sqNum.length; i++){
-            if(sqNum[i] == true){
+        for(int i = 2; i * i < n; i++){
+            if(sqNum[i]){
                 for(int j = 2; i * j < n ; j++){
                     sqNum[i * j] = false;
                 }
@@ -24,8 +26,8 @@ class Solution {
         }
         
         int countPrime = 0;
-        for(int i = 2; i <sqNum.length; i++){
-            if(sqNum[i] == true){
+        for(int i = 2; i <n; i++){
+            if(sqNum[i]){
                 countPrime++;
             }
         }
