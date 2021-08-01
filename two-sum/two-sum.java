25ccdc07aac0dd493943d3e916df217917e9d8c8
Ngu Jason <ngujason94@gmail.com>
2021-08-01 09:31:42 -0400

    Time: 1 ms (99.55%), Space: 39.2 MB (53.75%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,24 +1,22 @@
 /*
-to solve this problem 
-we traverse via array and and check 
-target = arr[i] + arr[j] 
-which implies aar[i] = target - aff[j]
-so if we find arr[i] in hmap we just retunr the index else we add into our hashmap
-
+we'll traverse tha array and input into the hashmap
+potential sume is num[i] + target
+so if map contains potential sum we return indices
+else we add into hashmap
 */
-
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-           int potentialMatch = target - nums[i];
+            int potentialMatch = target - nums[i];
             if(map.containsKey(potentialMatch)){
-                return new int[] {map.get(potentialMatch), i};
-            } else{
+                return new int[]{map.get(potentialMatch), i};
+            } else {
                 map.put(nums[i], i);
             }
         }
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
 }
