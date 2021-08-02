c5037b89d3046e00bbdb6805bf1fe0cefd862d28
Ngu Jason <ngujason94@gmail.com>
2021-08-02 00:42:55 -0400

    Time: 1 ms (99.55%), Space: 39.1 MB (76.00%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,8 +1,9 @@
 /*
-we'll traverse tha array and input into the hashmap
-potential sume is num[i] + target
-so if map contains potential sum we return indices
-else we add into hashmap
+to solve this question we need a hash map and match 
+match = target - arri, if match present in hashmap get the value
+else add into hashmap
+
+
 */
 
 class Solution {
@@ -12,11 +13,11 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             int potentialMatch = target - nums[i];
             if(map.containsKey(potentialMatch)){
-                return new int[]{map.get(potentialMatch), i};
-            } else {
+                return new int[] {map.get(potentialMatch), i};
+            }else {
                 map.put(nums[i], i);
             }
         }
-        return new int[]{-1, -1};
+        return new int[] {-1, -1};
     }
 }
