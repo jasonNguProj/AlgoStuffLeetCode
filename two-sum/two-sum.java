92913b9ff13ebcb3e8a76498f9abc803168dc868
Ngu Jason <ngujason94@gmail.com>
2021-08-06 02:10:12 -0400

    Time: 1 ms (99.57%), Space: 39.1 MB (63.79%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,15 +1,8 @@
-/*
-to solve this question we need a hash map and match 
-match = target - arri, if match present in hashmap get the value
-else add into hashmap
-
-
-*/
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        
         for(int i = 0; i < nums.length; i++){
             int potentialMatch = target - nums[i];
             if(map.containsKey(potentialMatch)){
@@ -18,6 +11,6 @@ class Solution {
                 map.put(nums[i], i);
             }
         }
-        return new int[] {-1, -1};
+        return null;
     }
 }
