7426c5b46f3643040aa127c2de64feb81c10f14e
Ngu Jason <ngujason94@gmail.com>
2021-08-05 22:11:29 -0400

    Time: 3 ms (85.96%), Space: 37.8 MB (62.11%) - LeetHub

--- partition-labels/partition-labels.java
+++ partition-labels/partition-labels.java
@@ -3,12 +3,13 @@ to solve this problem we will keep track of every xter ending
 and then update and take the max of a xter in a particular window
 when ever when we loop via the xter and then it reaches the max we just find the length and add 
 into our list
-subssequently we want to start at the 0 + 1 index for the next xter
-
+subsequently we want to start at the 0 + 1 index for the next xter
+we solve using O(N) time O(1) space since the letters of the alphabeth has a max of 26 xters
 
 
 
 */
+
 
 class Solution {
     public List<Integer> partitionLabels(String S) {
