8edf3d5eee36d7d14e040053f81283cd50adafcb
Ngu Jason <ngujason94@gmail.com>
2021-08-08 19:49:19 -0400

    Time: 2 ms (60.12%), Space: 39.5 MB (36.49%) - LeetHub

---
+++ add-two-numbers/add-two-numbers.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*
+to solve this question we first create a dummy node that will hold our res
+then we create a carry var
+and to calculate carry is sum/10
+and then to update the are where we have a car we do 
+dummy.next  = new ListNode(sum mod 10)
+in the end we return our dummy.next
+*/
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+        while(l1 != null || l2!= null | carry != 0){
+            if(l1!= null){
+                carry+=l1.val;
+                l1 = l1.next;
+            }
+            
+             if(l2!= null){
+                carry+=l2.val;
+                l2 = l2.next;
+            }
+            current.next = new ListNode(carry%10);
+            carry /=10;
+            current = current.next;
+        }
+        
+        return dummy.next;
+        
+    }
+}
