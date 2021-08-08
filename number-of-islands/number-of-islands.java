346f4bf4a63936153ae7ac1b2860fd02adf2cbcb
Ngu Jason <ngujason94@gmail.com>
2021-08-08 00:15:59 -0400

    Time: 2 ms (46.74%), Space: 44.7 MB (6.07%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -17,7 +17,7 @@ class Solution {
     
     public void dfs(char[][] grid, int i , int j){
         
-        if( i  < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] != '1')
+        if( i  < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == '0')
                 return;
         
         grid[i][j] = '0';
