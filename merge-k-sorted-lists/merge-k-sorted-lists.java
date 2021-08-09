af6054ed5adb42fe1bf9d1bacc1a9253ccc1de54
Ngu Jason <ngujason94@gmail.com>
2021-08-09 00:11:26 -0400

    Time: 5 ms (46.85%), Space: 40.3 MB (87.90%) - LeetHub

---
+++ merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/*
+to solve this problem we will use a minHeap
+that will help up to merge all list together and sort them 
+so we traverse via our listnode and then update position 
+then we traverse via our heap while its size is greter than xero we add into our dummy 
+then return our dummy.next 
+
+*/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(ListNode head : lists){
+            while(head != null){
+                   minHeap.offer(head.val);
+                head = head.next;
+            }
+        }
+        
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        while(minHeap.size() > 0){
+            current.next = new ListNode(minHeap.poll());
+            current = current.next;
+        }
+        return dummy.next;
+    }
+}
