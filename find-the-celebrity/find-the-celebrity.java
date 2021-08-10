8177a8c62a30fdca314571a6ad359e13858de578
Ngu Jason <ngujason94@gmail.com>
2021-08-10 03:13:50 -0400

    Time: 19 ms (72.55%), Space: 39.3 MB (58.39%) - LeetHub

--- find-the-celebrity/find-the-celebrity.java
+++ find-the-celebrity/find-the-celebrity.java
@@ -1,28 +1,39 @@
 /* The knows API is defined in the parent class Relation.
       boolean knows(int a, int b); */
-//so to solve this question given the knows api
-// we'll set a celeb varible to 0, and traverse via the people 
-// if knows(celeb, i) , i is the celeb
-// also we need to check when it will be false 
-// check if the dnt know themselselves, check if celeb a knos i , but i does not know celeb,
-//we return -1 theres no celeb
-//at the end return celeb
+
+/*
+to solve this question we are given a know api 
+that telss us if i knows j 
+then j is the celeb 
+and we are told to reduce calls to the API so we can find our celeb
+and then check if celeb is really our celeb
+*/
 
 public class Solution extends Relation {
+    int numOfPeople;
     public int findCelebrity(int n) {
+        
+        numOfPeople = n ;
         int celeb = 0;
-        
-        for(int i = 1; i < n; i++){
-            if(knows(celeb, i)){
+        for(int i = 0; i < n; i++){
+            if (knows(celeb, i))
                 celeb = i;
+        }
+         if(celebCheck(celeb)){
+                return celeb;
+            }
+        return -1;
+    }
+    
+    
+    public boolean celebCheck(int i){
+        for(int j = 0; j < numOfPeople; j++){
+            if( i == j) continue; // because we dont want to be doing a check with it self we are checking the other people // in a 
+            
+            if(knows(i, j) || !knows(j, i)){
+                return false;
             }
         }
-        
-        for(int i = 0; i < n ; i++){
-            if(celeb != i && knows(celeb, i) || !knows(i, celeb)){
-                return -1;
-            }
-        }
-        return celeb;
+        return true;
     }
 }
