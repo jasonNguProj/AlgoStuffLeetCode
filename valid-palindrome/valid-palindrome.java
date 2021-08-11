47931f589f094cac0b57c501eb750c177028071e
Ngu Jason <ngujason94@gmail.com>
2021-08-11 06:47:15 -0400

    Time: 2 ms (98.19%), Space: 39.2 MB (58.63%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.java
@@ -0,0 +1,34 @@
+/*
+to check if a fxn is a palindrome
+firstly the promt gives us that non digits are 
+parts of the strings and should not be counted 
+so we check that visa 2 pointers 
+and then when we hve ignored the non digits 
+we check is the front is not equal to last we return false 
+move our pointers inwards else return true 
+
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end){
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+                continue;
+            }
+            
+              if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) return false;
+            start++;
+            end--;   
+        }
+        return true ;
+    }
+}
