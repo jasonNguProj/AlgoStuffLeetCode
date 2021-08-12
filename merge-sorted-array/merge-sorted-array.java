beb7f9d7228d739c5143ce723904eba13063077d
Ngu Jason <ngujason94@gmail.com>
2021-08-12 04:30:40 -0400

    Time: 0 ms (100.00%), Space: 39.3 MB (29.12%) - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -13,23 +13,14 @@ and we will terminate when p2 is done
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-         int idxOne = m -1, idxTwo = n -1, idxUpdate = m + n -1;
-          while (idxTwo >=0){
-            if(idxOne >=0 && nums1[idxOne] > nums2[idxTwo]){
-                nums1[idxUpdate--] = nums1[idxOne--];
-            }else{
-                nums1[idxUpdate--] = nums2[idxTwo--];
+        int p1 = m -1, p2 = n - 1, p = m + n -1;
+        
+        while(p2 >= 0){
+            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+               nums1[p--] = nums1[p1--]; 
+            }else {
+                    nums1[p--] = nums2[p2--];       
             }
         }
-        
-//         int p1 = m -1, p2 = n - 1, p = m + n -1;
-        
-//         while(p2 >= 0){
-//             if(p1 >= 0 && nums1[p1] < nums2[p2]){
-//                 nums1[p--] = nums2[p2--];
-//             }else {
-//                   nums1[p--] = nums1[p1--];
-//             }
-//         }
     }
 }
