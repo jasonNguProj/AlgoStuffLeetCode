68b7cea35deb24b353a6b8771afbdb0b80ec860c
Ngu Jason <ngujason94@gmail.com>
2021-08-12 19:50:18 -0400

    Time: 1 ms (12.25%), Space: 39.8 MB (6.21%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -21,7 +21,7 @@ so we need to check that as well
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         
-        ListNode dummy = new ListNode();
+        ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         while(l1 != null && l2 != null){
             if(l2.val >= l1.val){
