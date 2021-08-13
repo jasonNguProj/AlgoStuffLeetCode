5f6843fc8ee764830647d97eca79ba3d31d12df4
Ngu Jason <ngujason94@gmail.com>
2021-08-13 01:19:38 -0400

    Time: 0 ms (100.00%), Space: 39.6 MB (5.15%) - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -17,9 +17,15 @@ class Solution {
         
         while(p2 >= 0){
             if(p1 >= 0 && nums1[p1] > nums2[p2]){
-               nums1[p--] = nums1[p1--]; 
+               nums1[p] = nums1[p1]; 
+                p--;
+                p1--;
+                
             }else {
-                    nums1[p--] = nums2[p2--];       
+                    nums1[p] = nums2[p2];  
+                
+                p--;
+                p2--;
             }
         }
     }
