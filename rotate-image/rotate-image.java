2aaaf1e58240657d1e99960571d51c5643a65678
Ngu Jason <ngujason94@gmail.com>
2021-08-17 21:26:15 -0400

    Time: 0 ms (100.00%), Space: 39.1 MB (66.49%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -2,47 +2,25 @@
 // the col become row
 // invert by going upto n/ 2 bound and swap inward because if we set bound past that 
 // it will revert to original state
-// class Solution {
-//     public void rotate(int[][] matrix) {
-        
-//         for(int i = 0; i < matrix.length; i++){
-//             for(int j = 0; j < matrix[i].length; j++){
-//                 int temp = matrix[i][j];
-//                 matrix[i][j] = matrix[j][i];
-//                 matrix[j][i] = temp;
-//             }
-//         }
-        
-//         for(int i = 0; i < matrix.length; i++){
-//             for(int j = 0; j < matrix.length/2; j++){
-//                 int temp = matrix[i][j];
-//                 matrix[i][j] = matrix[i][matrix.length -1 -j];
-//                 matrix[i][matrix.length -1 -j] = temp;
-                
-//             }
-//         }
-        
-//     }
-// }
-
-
-public class Solution {
+class Solution {
     public void rotate(int[][] matrix) {
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[i].length; j++){
-               // int temp = 0;
-               int  temp = matrix[i][j];
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for(int i =0 ; i<matrix.length; i++){
-            for(int j = 0; j<matrix[i].length/2; j++){
-               // int temp = 0;
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length/2; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j] = temp;
+                matrix[i][j] = matrix[i][matrix.length -1 -j];
+                matrix[i][matrix.length -1 -j] = temp;
+                
             }
         }
+        
     }
 }
