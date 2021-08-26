baeadb28ed01fea957171213ae5b50a3db5dadfc
Ngu Jason <ngujason94@gmail.com>
2021-08-26 02:09:23 -0400

    Time: 4 ms (16.57%), Space: 68.7 MB (5.07%) - LeetHub

--- max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -4,12 +4,15 @@ to solve this problem we have to maintian a window
 of k number of zeoes by the time the nums zeroes 
 is greater than k then we reduce the number of zeroes and
 shrink the window as well
+dnt forget edge cases, time is O(N) | O(1) space
 
 */
 
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int start =0, end =0, maxLen = 0, zeroCount = 0;
+        
+        if(nums.length == 0 || nums == null) return -1;
         
         while(end < nums.length){
             if(nums[end] == 0) zeroCount++;
