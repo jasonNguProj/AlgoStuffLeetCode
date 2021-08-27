690b29e076dfe7fd1018abb746f41a47507e7f41
Ngu Jason <ngujason94@gmail.com>
2021-08-27 04:04:15 -0400

    Time: 2 ms (10.91%), Space: 42 MB (5.33%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.java
+++ balanced-binary-tree/balanced-binary-tree.java
@@ -13,31 +13,34 @@
  *     }
  * }
  */
-/*
-point to note when u are trying to find diameter or height or length always take 
-into acc that at every node you are computing left and right and 
-trying to get the max and so the formula will always be 1 + (math.max(left, right))
-rather than thinking of adding 1 at each step you simply do 1 + Math.max(left, right)
 
-so the algorithm to solve this question is to initial a global variable to control the recurssion
-the 2 fxn one to cals height and the other to return if its a valid balanced tree or not
-so time is O(log n) on average beacuse we might not access the whole tree when we discover it is unbalanced 
-O(N) in the worst case if we traverse the whole tree
-and O(N) space in the worst case for a skewed tree and o(log n) in the av case for a balanced tree
+/*
+to solve this problem firstly we have to understand that we neeed the height
+because the height will give us the info of whether the tree is balanced or not 
+note than when ever u are asked depth, height , length to solve it basically 
+is to take max of the recursion on left and right + 1 
+because in essence math.max of left and right + 1 gets called on every level
+of the tree like basically behaves like recursion rather than adding up 1 at every level in the tree
+
+
 */
 class Solution {
     public boolean isBalanced(TreeNode root) {
+        
         if(root == null) return true;
-        if(Math.abs(computeHeight(root.left) - computeHeight(root.right)) > 1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
+        
+        if(Math.abs(computHeight(root.left) - computHeight(root.right)) > 1) return false;
+           
+           return isBalanced(root.left) && isBalanced(root.right);
+        
     }
     
-    public int computeHeight(TreeNode root){
+    public int computHeight(TreeNode root){
         if(root == null) return 0;
         
-        int left =  computeHeight(root.left);
-        int right = computeHeight(root.right);
+        int left = computHeight(root.left);
+        int right = computHeight(root.right);
         
-        return 1 + Math.max(left, right);
+        return Math.max(left, right) + 1;
     }
 }
