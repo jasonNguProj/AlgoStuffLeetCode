712a79acfc474fb276e87e48ac70979957cee751
Ngu Jason <ngujason94@gmail.com>
2021-08-28 04:48:13 -0400

    Time: 2 ms (96.38%), Space: 47.9 MB (51.62%) - LeetHub

--- count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,27 +13,41 @@
  *     }
  * }
  */
-/*to solve this question we have to undersatnd that in order for a node to be good
-the present value should be greater than or equal to the previous value 
-and then we just update our max and initialize our goodnode as a global variable to contol our recursion
+
+/*
+to solve this question we will use dfs recursion and traverse via the left and right node
+create a good node variable that we will increment whenever the present rootValue
+is greater or equal to the previous rootvalue then , 
+we take the max btw the prev and present max value to rep our new max value,
+then recursively call the fxn on the left and right sub tree pasiing in therie values
+so we will do it in 2 diff fxn , the dfs fxn and the main fxn and initialze the running val to Integer.MIN_VALUE
+and also initialize the goodnode variable as a class memebr on the global level and in the end return the goodNodes
+
+Time is O(N) because you trav the entire tree to find goo nodes
+O(D) space cus of recursive stack  of d depth of hw far the recursive stack goes deep.
 */
+
+
 class Solution {
-    
     int goodNode;
-    public int goodNodes(TreeNode root) {  
-       // goodNode = 0 ;
+    public int goodNodes(TreeNode root) {
+       // goodNode = 0;
         dfs(root, Integer.MIN_VALUE);
         return goodNode;
+        
     }
     
     private void dfs(TreeNode root, int max){
+        
         if(root == null) return;
         
         if(root.val >= max){
             goodNode++;
         }
-        max = Math.max(root.val, max);
+          max = Math.max(root.val, max);
         dfs(root.left, max);
-         dfs(root.right, max);
+        dfs(root.right, max);
     }
+    
+    
 }
