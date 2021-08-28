ed4d5c515197d72641ab8ad567fef5a273b78155
Ngu Jason <ngujason94@gmail.com>
2021-08-28 00:18:12 -0400

    Time: 8 ms (13.23%), Space: 93.1 MB (7.28%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -13,19 +13,21 @@
  *     }
  * }
  */
+/*
+this problem can be easily solved via bfs , but solving it
+via dfs recursion is to recurse on the left and right 
+and take the min of them and add one bcus at each level depth is 1
+time is O(N) | space is O(N) because of recursive stack
+
+*/
 class Solution {
     public int minDepth(TreeNode root) {
         
-//         if(root == null) return 0;
-        
-//         if(root.left == null) return minDepth(root.left) + 1;
-//          if(root.right == null) return minDepth(root.right) + 1;
-//         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-        
-        if(root==null){return 0;}
-	if(root.left==null){return minDepth(root.right) +1;}
-	if(root.right==null){return minDepth(root.left)+1;}
-	return Math.min(minDepth(root.left),minDepth(root.right))+1;
-        
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+          if (left == 0 || right == 0) 
+              return Math.max(left, right) + 1;
+        return Math.min(left, right) + 1;
     }
 }
