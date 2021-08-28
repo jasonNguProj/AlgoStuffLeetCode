1d9fe771524f9698a65071ad9255176bfd97663b
Ngu Jason <ngujason94@gmail.com>
2021-08-28 01:24:59 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (6.31%) - LeetHub

---
+++ path-sum/path-sum.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+to solve this our base case will be if the 
+root.val == 0 we return true that theres a path else false
+then we call our fxn on the left and right branch recursively
+
+*/
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if(root == null ) return false;
+        
+         if(root.left == null && root.right == null && targetSum - root.val == 0) return true;
+        
+        boolean left = hasPathSum(root.left, targetSum - root.val);
+        boolean right = hasPathSum(root.right, targetSum - root.val);
+        
+        return left || right;
+    }
+}
