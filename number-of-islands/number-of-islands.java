7c5ead051d550b8fc715f946caafdad8c294e316
Ngu Jason <ngujason94@gmail.com>
2021-08-31 00:55:35 -0400

    Time: 1 ms (99.95%), Space: 41.5 MB (42.91%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,29 +1,35 @@
 
+/*to solve this question we'll use dfs recursive to visit its neighbours when ever we 
+see a land 1 , when ever we see 0 we do nothing 
+time is O(N * M) ans space is O(N) */
+
 class Solution {
     public int numIslands(char[][] grid) {
         
-        int count = 0;
+        if(grid == null || grid.length == 0) return 0;
+        int islandCount = 0;
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
                     dfs(grid, i, j);
-                count++;
+                    islandCount++;
                 }
-                    
             }
         }
-        return count;
+        return islandCount;
     }
     
-    public void dfs(char[][] grid, int i , int j){
-        
-        if( i  < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == '0')
-                return;
-        
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i , j - 1);
-        dfs(grid, i , j + 1);
-    }
+     public void dfs(char[][] grid, int i, int j){
+         
+         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+             return;
+         
+         grid[i][j] = '0';
+         dfs(grid, i - 1, j);
+          dfs(grid, i + 1, j);
+          dfs(grid, i, j - 1);
+          dfs(grid, i, j + 1);
+         
+     }
 }
