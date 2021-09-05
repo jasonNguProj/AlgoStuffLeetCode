5bd3e6a84500b52a8a74642f602f4f2faffc74b3
Ngu Jason <ngujason94@gmail.com>
2021-09-04 21:05:33 -0400

    Time: 2 ms (95.57%), Space: 39.5 MB (31.69%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.java
+++ intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,42 +1,31 @@
 /*
-to solve this problem we will put a ptr on bith arrays starting nums
-and move the pointers when ever we found that the values in that pointer 
-is equal we add it into our array 
-
+another way of solving this problem is by using 2 hash set
+1 for adding array 1 elelments into it and another for keeping the 
+elements if they are thesame then we return the array
 */
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         
-         Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        
-       Set<Integer> result = new HashSet<>();
-        
-        int idxOne = 0, idxTwo = 0;
-        
-       
-        while (idxOne < nums1.length && idxTwo < nums2.length){
-            
-            if(nums1[idxOne] == nums2[idxTwo]){
-                result.add(nums1[idxOne]);
-                idxOne++;
-                idxTwo++;
-            } else if (nums1[idxOne] > nums2[idxTwo]){
-                idxTwo++;
-            } else if (nums1[idxOne] < nums2[idxTwo]){
-                idxOne++;
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums1){
+            set.add(n);
         }
-    }
-        int[] intersect = new int[result.size()];
         
+        Set<Integer> intersect = new HashSet<>();
+        for(int i : nums2){
+            if(set.contains(i)){
+                intersect.add(i);
+            }
+        }
+        
+        int[] result = new int[intersect.size()];
         int idx =0;
-        for(int num : result){
-            intersect[idx] = num;
+        
+        for(int j : intersect){
+            result[idx] = j;
             idx++;
         }
-       
-        
-        return intersect;
+        return result;
     }
 }
