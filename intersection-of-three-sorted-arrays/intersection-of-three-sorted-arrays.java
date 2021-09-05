694440e9ddbb06e7bf39e44d99d2ac293e8bd6cd
Ngu Jason <ngujason94@gmail.com>
2021-09-05 01:33:47 -0400

    Time: 1 ms (100.00%), Space: 39.7 MB (50.49%) - LeetHub

--- intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
+++ intersection-of-three-sorted-arrays/intersection-of-three-sorted-arrays.java
@@ -1,37 +1,32 @@
 /*
-to solve this problem we will use three sets and then traverse
-via the set and then add the numbers in a list and then return that list
+
+since the array is sorted we can solve this qxn with three ponters
+for array 1, 2 and 3, and manipulate them
 
 */
 
 class Solution {
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
         
-        Set<Integer> set = new HashSet<>();
-        for(int i : arr1){
-            set.add(i);
-        }
+        int idx = 0, idxOne = 0, idxTwo = 0;
         
-        Set<Integer> setTwo =new HashSet<>();
-        for(int j : arr2){
-            if(set.contains(j)){
-                setTwo.add(j);
+        List<Integer> result = new ArrayList<>();
+        while(idx < arr1.length && idxOne < arr2.length && idxTwo < arr3.length){  
+            if(arr1[idx] == arr2[idxOne] && arr2[idxOne] == arr3[idxTwo]){
+                result.add(arr1[idx]);
+                idx++;
+                idxOne++;
+                idxTwo++;    
+            }
+            else if(arr1[idx] < arr2[idxOne]) {
+                idx++;
+            }   else if(arr2[idxOne] < arr3[idxTwo]) {
+                idxOne++;
+            } else{
+                idxTwo++;
             }
         }
-         Set<Integer> setThree =new HashSet<>();
-        for(int k : arr3){
-            if(setTwo.contains(k)){
-                setThree.add(k);
-            }
-        }
-        
-        LinkedList<Integer> result = new LinkedList<>();
-        
-        for(Integer num : setThree){
-            result.add(num);
-        }
-        
-        Collections.sort(result);
         return result;
+        
     }
 }
