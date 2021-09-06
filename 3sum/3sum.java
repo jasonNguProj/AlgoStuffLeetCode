aac377c50d1addcb6a6845ecf02c98204f7bf833
Ngu Jason <ngujason94@gmail.com>
2021-09-06 00:37:48 -0400

    Time: 468 ms (19.37%), Space: 43.1 MB (56.10%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -2,7 +2,7 @@
 to solve this problem we need 3 pointers
 that we will keep manipulating as we traverse the array
 and then add into a result list, and at the end return our result
-
+O(N ^ 2) Time | O(N) space 
 
 */
 
@@ -10,6 +10,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {     
         Arrays.sort(nums);
         Set<List<Integer>> result = new HashSet<>();
+        if (nums.length == 0 || nums == null) return new ArrayList<>(result);
         for(int i = 0; i < nums.length - 2; i++){
             int start = i + 1, end = nums.length - 1;
             while(start < end){
