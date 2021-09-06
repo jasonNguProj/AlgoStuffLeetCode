53514e4ce1a590cec6fa03d07dc1ced1b9c087f5
Ngu Jason <ngujason94@gmail.com>
2021-09-05 23:35:21 -0400

    Time: 495 ms (15.25%), Space: 43.4 MB (41.83%) - LeetHub

---
+++ 3sum/3sum.java
@@ -0,0 +1,30 @@
+/*
+to solve this problem we need 3 pointers
+that we will keep manipulating as we traverse the array
+and then add into a result list, and at the end return our result
+
+
+*/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {     
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            int start = i + 1, end = nums.length - 1;
+            while(start < end){
+                int tripletSum = nums[i] + nums[start] + nums[end];
+                if(tripletSum == 0){
+                    result.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                } else if(tripletSum > 0){
+                    end--;
+                }else if(tripletSum < 0){
+                    start++;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
