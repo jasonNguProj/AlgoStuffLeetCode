e141db7e485fc29bac5812c38b3d3cca35ca6b31
Ngu Jason <ngujason94@gmail.com>
2021-09-10 08:10:02 -0400

    Time: 4 ms (42.58%), Space: 52.6 MB (11.22%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,43 +1,40 @@
 /*
-firstly when u hear of a qxn asking for possible paths
-that should be a hint that you should use backtracking 
-as u have to explore a node and its node as 1 solution
-and then bactrack from that neighbor inorder to explore 
-the other neighbor, 
-so in this question we will have a path var that will keep track of paths
-to mimic a visited flag of nodes visited then call our fxn recursively to visit the neighbors 
-deep then inorder to visit other neighbors we backtrack from that 
-and remember that u need to assume 0 as start node
-
-then in the end when we reach the end we just add our visited nodes to our result
+to solve this question i will use dfs recursive 
+via backtracking
 
 
 
 */
 
+
+
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
+        List<List<Integer>> res = new ArrayList<>();
         
-        List<List<Integer>> result = new ArrayList<>();
-        if(graph.length == 0) return result;
-        List<Integer> path = new ArrayList<>();
-        path.add(0);
-        dfs(graph, 0, path, result);    
-        return result;
+        if(graph.length == 0 || graph == null) return res;
+            List<Integer> path = new ArrayList<>();
+                path.add(0);
+        dfs(res, path, 0, graph);
+        return res;
     }
     
-    private void dfs(int[][] graph, int start,  List<Integer> path, List<List<Integer>> result){
+    private void dfs(List<List<Integer>> res, List<Integer> path, int start, int[][] graph){
+        
         
         if(start == graph.length - 1){
-            result.add(new ArrayList<>(path));
+            res.add(new ArrayList<>(path));
             return;
         }
         
         for(int n : graph[start]){
             path.add(n);
-            dfs(graph, n, path, result);
+            dfs(res, path, n, graph);
             path.remove(path.size() - 1);
+            
         }
+        
+        
     }
 }
