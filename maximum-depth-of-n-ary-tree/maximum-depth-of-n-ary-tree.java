f124c3902f9eddf9ff26fc1cdf82548a8df34835
Ngu Jason <ngujason94@gmail.com>
2021-09-14 21:30:49 -0400

    Time: 3 ms (9.41%), Space: 41.6 MB (5.02%) - LeetHub

--- maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
+++ maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
@@ -1,17 +1,46 @@
 /*
 // Definition for a Node.
-3
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
 */
 
+/*
+to solve this qxn optimaly well use bfs
+this is an narry tree so the children a list of nodes
+we'll traverse them and add in or queue
+*/
 class Solution {
     public int maxDepth(Node root) {
         
-        if(root == null) return 0;
-        int depth =0;
-        for(Node child : root.children){
-            depth = Math.max(depth, maxDepth(child));
+        if (root == null) return 0;
+        int depth = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+             depth++;
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                 Node node = queue.poll();
+            for(Node child : node.children){
+                queue.offer(child);
+            }
+            }
+           
         }
-        return depth + 1;
+        return depth;
         
     }
 }
