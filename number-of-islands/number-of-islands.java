231417d74d0bb1ffc63ef88d7cf1725398e80135
Ngu Jason <ngujason94@gmail.com>
2021-09-20 01:29:49 -0400

    Time: 4 ms (34.73%), Space: 55.8 MB (9.34%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -9,10 +9,14 @@ class Solution {
         if(grid == null || grid.length == 0) return 0;
         int islandCount = 0;
         
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
+        int rowlength = grid.length;
+        int colLength = grid[0].length;
+        boolean[][] visited = new boolean[rowlength][colLength];
+        
+        for(int i = 0; i < rowlength; i++){
+            for(int j = 0; j < colLength; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    dfs(grid, visited, i, j);
                     islandCount++;
                 }
             }
@@ -20,16 +24,16 @@ class Solution {
         return islandCount;
     }
     
-     public void dfs(char[][] grid, int i, int j){
+     public void dfs(char[][] grid, boolean visited[][], int i, int j){
          
-         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] == true || grid[i][j] == '0')
              return;
          
-         grid[i][j] = '0';
-         dfs(grid, i - 1, j);
-          dfs(grid, i + 1, j);
-          dfs(grid, i, j - 1);
-          dfs(grid, i, j + 1);
+         visited[i][j] = true;
+         dfs(grid, visited, i - 1, j);
+          dfs(grid, visited, i + 1, j);
+          dfs(grid, visited, i, j - 1);
+          dfs(grid,visited, i, j + 1);
          
      }
 }
