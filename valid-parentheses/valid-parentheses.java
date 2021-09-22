127d8774fe7f0097b010c0c4cb4da25149d38426
Ngu Jason <ngujason94@gmail.com>
2021-09-21 21:54:15 -0400

    Time: 1 ms (98.95%), Space: 37.1 MB (70.46%) - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,15 +1,29 @@
+
+/*
+to solve this problem we need to understand that we're trying to 
+match brakets , and a perfect data structure for this 
+will be the stack where we match our present elemnt with the element at the peek 
+then we return true if at the end our stack is empty because
+in essense our stack needs to be empty inorder for the input to be valid 
+we use deque over stack because deque is an interface, acts like a stack, 
+stack is also inconsistent as you can acces an element by index which is inconsistent
+
+
+
+*/
+
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        
+        Deque<Character> queue = new ArrayDeque<>();
+        
         for(char c : s.toCharArray()){
-            if(st.isEmpty()) st.push(c);
-            else if(c == ')' && st.peek() == '(') st.pop();
-            else if(c == '}' && st.peek() == '{') st.pop();
-            else if(c == ']' && st.peek() == '[') st.pop();
-            else st.push(c);
+            if(queue.isEmpty()) queue.offer(c);
+            else if(c == ')'  && queue.peek() == '(') queue.poll();
+             else if(c == '}'  && queue.peek() == '{') queue.poll();
+             else if(c == ']'  && queue.peek() == '[') queue.poll();
+             else queue.push(c);
         }
-        
-        return st.isEmpty() ? true : false;
-        
+          return queue.isEmpty() ? true : false;
+        } 
     }
-}
