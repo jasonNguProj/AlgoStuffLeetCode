7e3d624e244b1f2fca9afa4e2b8172f0c3793319
Ngu Jason <ngujason94@gmail.com>
2021-09-25 16:21:59 -0400

    Time: 2 ms (71.31%), Space: 51.8 MB (93.88%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,12 @@
-/*to solve this problem we will use kadanse algo in one pass
-where we reset our curMax to 0 when ever we get a negative value
-and then keep track of overall max value and then return that value
-then time is O(N) | Space is O(1) */
-
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int curMax = 0;
-        int max = 0;
-        
+        int current =0, maxProfit = 0;
         for(int i = 1; i < prices.length; i++){
-            curMax = Math.max(0, curMax += prices[i] - prices[i - 1]);
-            max = Math.max(curMax, max);
+            current = Math.max(0, current += prices[i] - prices[i - 1]);
+            maxProfit = Math.max(current, maxProfit);
         }
-        return max;
+        
+        return maxProfit;
     }
 }
