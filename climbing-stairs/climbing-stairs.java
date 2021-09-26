3bafc04c3f01b8a2f69612b25002c3eba7b3a94f
Ngu Jason <ngujason94@gmail.com>
2021-09-26 00:15:56 -0400

    Time: 0 ms (100.00%), Space: 37.6 MB (25.80%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,16 +1,25 @@
+/*
+to solve this question we need to understand that its
+a fibbonnaci variation question given that if n = 1 we return 1
+and if nis 2 we return 2 so we will be solving it same way we do with fibonacci number
+
+
+*/
+
+
 class Solution {
     public int climbStairs(int n) {
         
         
         if(n == 1) return 1;
-        int first = 1;
-        int second = 2;
         
-        for(int i = 3; i <= n; i++){
-            int third = first + second;
-            first = second;
-            second = third;
+        int firstNum = 1, secNum = 2;
+        
+        for(int i = 3; i <= n ; i++){
+            int thirdNum = firstNum + secNum;
+            firstNum = secNum;
+            secNum = thirdNum;    
         }
-        return second;
+        return  n > 1 ?  secNum : 1;
     }
 }
