3750471109542c6c84833b251a0e592ae98b6dfd
Ngu Jason <ngujason94@gmail.com>
2021-09-29 05:34:58 -0400

    Time: 54 ms (45.60%), Space: 119.8 MB (38.52%) - LeetHub

--- lru-cache/lru-cache.java
+++ lru-cache/lru-cache.java
@@ -1,61 +1,56 @@
-/*
-to solve this question we will use a hashMap and a doubly linkedlist
-our doublie ll will have a head and a tail
-so like       Next
-< null---head ---> tail---> null
-              <prev--   
-so we will have a node class that will have our node and tail
-and we need to implement insert and remove fxn of our nod e
-then we will use our remove and insert in ou LRU cache operations
-we use hasp map and linked list because the remove and insert in ll 
-is O(1) operation manipuling pointers similary the get and put fxn of map
-is O(1)
-*/ 
 
+/*
+to solve this qxn i will use a hmap which has O(1) get and put
+similary a linked list has O(1) insert and remove by maniulating pointers
+
+head  <---prev-- taiL
+      ----next-->
+*/
 
 class Node{
-    int key, value;
-    Node head, tail;
-    Node prev, next;
+    Node prev, next, head, tail;
+    int value, key;
     Node(int key, int value){
         this.key = key;
         this.value = value;
     }
 }
-class LRUCache {
-    int capacity;
-    Node head = new Node(0, 0);
-    Node tail = new Node(0, 0);
-    Map<Integer, Node>   map = new HashMap<>();
 
+class LRUCache {
+
+    int capacity;
+    Map<Integer, Node> map;
+    Node head = new Node(0, 0);
+     Node tail = new Node(0, 0);
     public LRUCache(int capacity) {
-         this.capacity = capacity;
-        tail.prev =head;
-        head.next = tail;
+         map = new HashMap<>();
+        this.capacity = capacity;
+         head.next = tail;
+         tail.prev = head;
     }
     
-    public int get(int key) {
-        if(map.containsKey(key)){
-            Node node = map.get(key);
-            remove(node);
-            insert(node);
-            return node.value;
-        }else {
-            return -1;
-        }
-        
+     public int get(int key) {
+         if(map.containsKey(key)){
+             Node node = map.get(key);
+             remove(node);
+             insert(node);
+             return node.value;
+         } else {
+             return -1;
+         }
     }
     
     public void put(int key, int value) {
-        
         if(map.containsKey(key)){
-            remove(map.get(key));
+           Node node = map.get(key); 
+            remove(node);
         }
         
-        if(map.size() == capacity){
+        if(map.size() >= capacity){
             remove(tail.prev);
         }
-        insert(new Node(key , value));
+        insert(new Node(key, value));
+        
     }
     
     public void insert(Node node){
@@ -66,12 +61,16 @@ class LRUCache {
         headNext.prev = node;
         node.next = headNext;
     }
+    
      public void remove(Node node){
-         map.remove(node.key);
+        map.remove(node.key);
          node.prev.next = node.next;
          node.next.prev = node.prev;
-        
+         
     }
+   
+    
+    
 }
 
 /**
