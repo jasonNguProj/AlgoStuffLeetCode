4a54487abda0d4e0df92d8db7fb52f164309894b
Ngu Jason <ngujason94@gmail.com>
2022-09-07 17:53:23 -0400

    Time: 687 ms (5.86%), Space: 41.8 MB (5.02%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,12 +1,21 @@
 
 /*
-
+solving via tabulation
 we need an dp array plus 1 , we need to save the base case
 then set the so fib of 0 is 0
 then we fill up the array with the first values and then
 fill up the rest with the fib formul which the sum of previous 2 numbers
 O(N) time and space
 
+
+solving via memoization 
+where we start form top and then go down 
+we first have our base case since memoization includes recursion
+
+we use a map to store the first two fib
+then we check if our map contains the fib , if it doens we 
+compute the fib and add as value, or cache into our map
+then we just return our fib value which essentially the value in our map
 
 */
 
@@ -15,17 +24,38 @@ O(N) time and space
 class Solution {
     public int fib(int n) {
         
-        if(n == 0) return 0;
+       // base case
         
-        int[] dp = new int[n + 1];
+        if (n <= 1) return n;
         
-        dp[0] = 0;
-        dp[1] = 1;
         
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i -2];
+        // cache some values we already know, so basically sub problems
+        //we use a hMap for these 
+        Map<Integer, Integer> map = new HashMap<>(Map.of(0, 0, 1, 1));
+        
+        
+        // then we check our map if we do have the cached value
+        //if we do we just return the value from our map
+        //if not we return the cached value
+        
+        if(!map.containsKey(n)){
+            
+            int fibValue = fib(n-1) + fib(n-2);
+            map.put(n, fibValue);
         }
+        return map.get(n);
         
-        return dp[n];
+//         if(n == 0) return 0;
+        
+//         int[] dp = new int[n + 1];
+        
+//         dp[0] = 0;
+//         dp[1] = 1;
+        
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i - 1] + dp[i -2];
+//         }
+        
+//         return dp[n];
     }
 }
